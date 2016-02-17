dce155535f8301aa1c38544005635876daef6f51
MitsukoLawL <mitsuko_uchiwa@hotmail.fr>
2016-02-17 18:47:43 +0100

    .

---
+++ spoon-examples-copy/src/main/java/minimal/DoSomeAction.java
@@ -0,0 +1,15 @@
+package minimal;
+
+/**
+ * Dummy class
+ *
+ * @author Sascha Tayefeh
+ */
+public class DoSomeAction {
+  public int actionFirst() {
+    return 3+2;
+  }
+  public int actionSecond() {
+    return 0;
+  }
+}
