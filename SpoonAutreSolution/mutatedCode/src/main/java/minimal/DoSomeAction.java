f0c8085b628afed59429ef2f368f4764c5abc1df
Alicia MARIN <ma202240@etu.unice.fr>
2016-02-23 16:28:13 +0100

    arbo

--- SpoonAutreSolution/mutatedCode/src/main/java/minimal/DoSomeAction.java
+++
@@ -1,9 +0,0 @@
-package minimal; 
-public class DoSomeAction {
-    protected int a;
-
-    int calc() {
-        a = 3;
-        return (a) - 4;
-    }
-}
---
+++ mutatedCode/src/main/java/minimal/DoSomeAction.java
@@ -0,0 +1,9 @@
+package minimal; 
+public class DoSomeAction {
+    protected int a;
+
+    int calc() {
+        a = 3;
+        return (a) + 4;
+    }
+}
